3329c39bad9b39896721da1a08ced5d104ef2553
evandrocoan <evandrocoan@hotmail.com>
2018-01-14 15:30:09 -0200

    Fixed the log.set_file_logger() name to log.setup_logger()

--- BufferScroll.py
+++ BufferScroll.py
@@ -30,7 +30,7 @@ from python_debug_tools import getLogger
 # Debugger settings: 0 - disabled, 127 - enabled
 log = getLogger( 127, os.path.basename( __file__ ) )
 
-#log.set_file_logger( "Debug.txt" )
+#log.setup_logger( "Debug.txt" )
 #log.clear_log_file()
 
 # log( 1, "Debugging" )
